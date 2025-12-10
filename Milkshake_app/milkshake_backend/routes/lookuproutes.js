af15de0f25242c3df3450ee50c5a85af2b30bf98
Sam Tinashe Maraya <marayasam9@gmail.com>
2025-12-10 15:39:40 +0200

    Delete Milkshake_app directory

--- Milkshake_app/milkshake_backend/routes/lookuproutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const lookupController = require("../controllers/lookupController");
-
-router.get("/flavours", lookupController.getFlavours);
-router.get("/toppings", lookupController.getToppings);
-router.get("/consistency", lookupController.getConsistency);
-
-module.exports = router;
